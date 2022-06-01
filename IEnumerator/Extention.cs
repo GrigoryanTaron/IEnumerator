f98e107298414e9806767a9a3e7d04d6f1a9ef40
Taron Grigoryan <1991taron@gmail.com>
2022-06-01 15:09:41 +0400

    Add project files.

---
+++ IEnumerator/Extention.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public static class Extention
+{
+    public static IEnumerable<int> Test(this IEnumerable<int> source,int dummy)
+    {
+        foreach (var item in source)
+        {
+            if (item > dummy)
+                yield return item;
+        }
+    }
+
+    public static IEnumerable<int> Add(this IEnumerable<int> soure)
+    {
+        foreach (var item in soure)
+        {
+            yield return item + 5;
+        }
+    }
+    public static IEnumerable<int> Add1(this IEnumerable<int> soure)
+    {
+        List<int> lll = new List<int>();
+        foreach (var item in soure)
+        {
+            lll.Add(item + 5);
+        }
+        return lll;
+    }
+    public static IEnumerable<int> Test1(this IEnumerable<int> source, int dummy)
+    {
+        List<int> l = new List<int>();
+        foreach (var item in source)
+        {
+            if (item > dummy)
+                l.Add(item);
+        }
+        return l;
+    }
+    public static IEnumerable<int> Multiple(this IEnumerable<int>source,int dummy)
+    {
+        List<int>list=new List<int>();
+        foreach (var item in source)
+        {
+            yield return item * dummy;
+        }
+    }
+    public static IEnumerable<int> Multiple1(this IEnumerable<int> source, int dummy)
+    {
+        List<int>list1=new List<int>();
+        foreach(var item in source)
+        {
+            list1.Add(item*dummy);
+        }
+        return list1;
+    }
+    public static IEnumerable<int> Div(this IEnumerable<int> source, int dummy)
+    {
+        List<int> list = new List<int>();
+        foreach (var item in source)
+        {
+            yield return item /dummy;
+        }
+    }
+
+    public static IEnumerable<int> Div1(this IEnumerable<int> source, int dummy)
+    {
+        List<int> list1 = new List<int>();
+        foreach (var item in source)
+        {
+            list1.Add(item / dummy);
+        }
+        return list1;
+    }
+
+
+    //TO DO
+    //public static IEnumerable<int> _Select(this IEnumerable<int> source, Func<int, int> selector)
+    //{
+    //    if (source == null) throw new ArgumentExeptionNull("source");
+    //    if (selector == null) throw new ArgumentExeptionNull("selector");
+
+    //    return new WhereSelectEnumerableIterator<TSource, TResult>(source, null, selector);
+    //}
+}
+
---
+++ IEnumerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> res = new List<int> { 5, 21, 12, 6, -18, -59, 31, 3, 2, 4 };
+            var finalllllll = res.ToList();
+            var enumerator = finalllllll.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //}
+
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //}
+            var res1 = res.Test1(6).Add1().First();
+            res1 = res.Test(-20).Add().First();
+            var res2 = res.Test(0).Add1();
+
+            foreach (int item in res2)
+            {
+                Console.WriteLine($"{item}");
+            }
+            var res3 = res.Test(0).Multiple(2);
+            foreach (int item in res3)
+            {
+                Console.WriteLine($"{item}");
+            }
+            var res4 = res.Test(20).Multiple1(2);
+            foreach (int item in res4)
+            {
+                Console.WriteLine($"{item}");
+            }
+            var res5 = res.Test(1).Div(2);
+            foreach (int item in res5)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+
+}
